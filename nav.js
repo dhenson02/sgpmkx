3e2defdd75c8749bcbcea6414a1ca26cfd03b19c
Deryck Henson <deryck.henson@gmail.com>
2015-08-26 20:40:42 -0500

    link to / (home) nested inside div.header

--- nav.js
+++ nav.js
@@ -4,18 +4,20 @@ var h = require("virtual-dom/h"),
 function render ( fhm, comm ) {
   return (
     h("div#navWrap.nav", [
-      h("a.header", {
-        "href": "#/"
-      }, [
-        h("div.logo", [
-          h("img", {
-            "src": "/kj/kx7/PublicHealth/SiteAssets/Pages/landing/phLogo48-gs.png",
-            "alt": "Public Health Home",
-            "height": "48",
-            "width": "48"
-          })
-        ]),
-        h("p.text", [ "Public Health", h("br"), h("small", [ "US Air Force" ]) ])
+      h("div.header", [
+        h("a", {
+          "href": "#/"
+        }, [
+          h("div.logo", [
+            h("img", {
+              "src": "/kj/kx7/PublicHealth/SiteAssets/Pages/landing/phLogo48-gs.png",
+              "alt": "Public Health Home",
+              "height": "48",
+              "width": "48"
+            })
+          ]),
+          h("p.text", [ "Public Health", h("br"), h("small", [ "US Air Force" ]) ])
+        ])
       ]),
       h("ul", [
         h("li", [
