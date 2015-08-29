a42c1e7dfa45467d12756fda1ee76fb0212cfaf7
Deryck Henson <deryck.henson@gmail.com>
2015-08-28 21:37:49 -0500

    code styling and added css file for tabs

--- tabs.js
+++ tabs.js
@@ -1,21 +1,25 @@
 var h = require("virtual-dom/h");
 
-function render() {
+function render( style ) {
   return (
     h("ul#contentTabs", [
       h("li.tab", [
         h("a", {
-          href: window.location.hash + "?tab=Policy"
+          href: window.location.hash + "?tab=Policy",
+          style: style
         }, [ "Policy" ])
       ]),
       h("li.tab", [
         h("a", {
-          href: window.location.hash + "?tab=Utilities"
+          href: window.location.hash + "?tab=Utilities",
+          style: style
         }, [ "Utilities" ])
       ]),
-      h("li.tab", [ h("a", {
-        href: window.location.hash + "?tab=Community"
-      }, [ "Contributions" ])
+      h("li.tab", [
+        h("a", {
+          href: window.location.hash + "?tab=Community",
+          style: style
+        }, [ "Contributions" ])
       ])
     ])
   );
